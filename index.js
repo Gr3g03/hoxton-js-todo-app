b5ecd6ee8124aa589c5216662426863e5e39e567
Gr3g03 <71180086+Gr3g03@users.noreply.github.com>
2021-11-25 14:51:19 +0100

    functions added

--- index.js
+++ index.js
@@ -3,3 +3,44 @@
 // 2. render the app based on the state
 // 3. update the state
 // 4. rerender the app based on the new state
+
+
+const todoState = {
+    todos : {
+        name: 'potato' ,
+        condition : true
+    }
+
+}
+
+function todoList(todoState){
+    const todoFormSection = document.querySelector('.add-todo-section')
+
+    const todoForm = document.querySelector('.add-item')
+
+    const addTodo = document.querySelector('.text-input')
+   
+    const todobuttoEL = document.querySelector('.submit-button')
+     
+    todoForm.append(addTodo, todobuttoEL )
+    todoFormSection.append(todoForm)
+
+}
+// todoList(todoState)
+
+
+function todoSection(todoState){
+    const todoListSection = document.querySelector('.todo-section')
+    const todoListUl = document.querySelector('.todo-list')
+
+    todoListSection.append(todoListUl)
+}
+
+
+function todoCompledet(todoState){
+    const todoCompletedSection = document.querySelector('.completed-section')
+    const todoCompletedULSection = document.querySelector('.completed-list')
+    todoCompletedSection.append(todoCompletedULSection)
+}
+
+
